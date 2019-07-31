de974afd79f6e7f4dc91b6cd1cba8fee2ad30606
shassen <shassen@tamu.edu>
2019-07-31 02:37:59 -0500

    added linear acceleration to myPlotter

--- myPlotter.py
+++ myPlotter.py
@@ -76,6 +76,9 @@ for i in range(len(listdir_)):
 	throttleout_ndx = manualthrottle_ndx = returnPltPos(name_,"_parsed_tx_accel_rpt_msg.command")
 	brakeout_ndx = returnPltPos(name_,"_parsed_tx_brake_rpt_msg.command")
 
+	linear_acc = np.gradient(data_[vecvelocity_ndx][:,1],0.02)
+	linear_acc_time = np.array(data_[vecvelocity_ndx][:,0])
+	linear_acc_ndx = np.column_stack((linear_acc_time, linear_acc))
 
 	try:	
 		output_file(listdir_[i][2:-1]+".html")
@@ -91,8 +94,10 @@ for i in range(len(listdir_)):
 		p3 = figure(title=listdir_[i], x_axis_label='time', y_axis_label='velocity (m/s) or throttle/brake %')
 		p3.line(data_[vecvelocity_ndx][:,0], data_[vecvelocity_ndx][:,1], legend="VectorNav Velocity",line_width=2, line_color="red")
 		p3.line(data_[throttle_ndx][:,0], data_[throttle_ndx][:,1], legend="Throttle Command",line_width=2, line_color="blue")
-		p3.line(data_[throttleout_ndx][:,0], data_[throttleout_ndx][:,1], legend="Throttle Output",line_width=2, line_color="cyan")
-		p3.line(data_[pacvelocity_ndx][:,0], data_[pacvelocity_ndx][:,1], legend="PACMod Velocity",line_width=2, line_color="green")
+		p3.line(linear_acc_ndx[:,0], linear_acc_ndx[:,1], legend="Linear Acceleration",line_width=2, line_color="cyan")
+
+		# p3.line(data_[throttleout_ndx][:,0], data_[throttleout_ndx][:,1], legend="Throttle Output",line_width=2, line_color="cyan")
+		# p3.line(data_[pacvelocity_ndx][:,0], data_[pacvelocity_ndx][:,1], legend="PACMod Velocity",line_width=2, line_color="green")
 		# p3.line(data_[brake_ndx][:,0], data_[brake_ndx][:,1], legend="Brake Command",line_width=2, line_color="green")
 		# p3.line(data_[brakeout_ndx][:,0], data_[brakeout_ndx][:,1], legend="Brake Output",line_width=2, line_color="black")
 
