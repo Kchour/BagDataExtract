bd59d69da1897285f781e6142e52133e13a56005
shassen <shassen@tamu.edu>
2019-08-20 12:53:46 -0500

    dr. swami showing data

--- myPlotter.py
+++ myPlotter.py
@@ -100,18 +100,18 @@ for i in range(len(listdir_)):
 
 		p3 = figure(title=listdir_[i], x_axis_label='time', y_axis_label='velocity (m/s)')
 		p3.line(data_[vecvelocity_ndx][:,0], data_[vecvelocity_ndx][:,1], legend="VectorNav Velocity",line_width=2, line_color="red")
-		p3.line(data_[pacvelocity_ndx][:,0], data_[pacvelocity_ndx][:,1], legend="PACMod Velocity",line_width=2, line_color="blue")
+		# p3.line(data_[pacvelocity_ndx][:,0], data_[pacvelocity_ndx][:,1], legend="PACMod Velocity",line_width=2, line_color="blue")
 		# p3.line(data_[manualthrottle_ndx][:,0], data_[manualthrottle_ndx][:,1], legend="Manual Throttle",line_width=2, line_color="blue")
-		# p3.line(linear_acc_ndx[:,0], linear_acc_ndx[:,1], legend="Linear Acceleration",line_width=2, line_color="cyan")
-		# p3.line(linear_acc_filter_ndx[:,0], linear_acc_filter_ndx[:,1], legend="Filtered Linear Acceleration",line_width=2, line_color="blue")
-		# p3.line(data_[brake_ndx][:,0], data_[brake_ndx][:,1], legend="Brake Command",line_width=2, line_color="green")
+		p3.line(linear_acc_ndx[:,0], linear_acc_ndx[:,1], legend="Linear Acceleration",line_width=2, line_color="cyan")
+		p3.line(linear_acc_filter_ndx[:,0], linear_acc_filter_ndx[:,1], legend="Filtered Linear Acceleration",line_width=2, line_color="blue")
+		p3.line(data_[brake_ndx][:,0], data_[brake_ndx][:,1], legend="Brake Command",line_width=2, line_color="green")
 		# p3.line(data_[brakeout_ndx][:,0], data_[brakeout_ndx][:,1], legend="Brake Output",line_width=2, line_color="black")
 		
-		p4 = figure(title=listdir_[i], x_axis_label='time', y_axis_label = 'Brake %')
-		p4.line(data_[brakeout_ndx][:,0], data_[brakeout_ndx][:,1]*100, legend="Brake Output",line_width=2, line_color="red")
-		p4.line(data_[brake_ndx][:,0], data_[brake_ndx][:,1]*100, legend="Brake Command",line_width=2, line_color="blue")
+		# p4 = figure(title=listdir_[i], x_axis_label='time', y_axis_label = 'Brake %')
+		# p4.line(data_[brakeout_ndx][:,0], data_[brakeout_ndx][:,1]*100, legend="Brake Output",line_width=2, line_color="red")
+		# p4.line(data_[brake_ndx][:,0], data_[brake_ndx][:,1]*100, legend="Brake Command",line_width=2, line_color="blue")
 
-		curr = gridplot([[p1, p3, p4]])
+		curr = gridplot([[p1, p3, p2]])
 		show(curr)
 
 
