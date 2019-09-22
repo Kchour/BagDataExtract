3aa09f0a12a573f1347afba36020c0afee7ad741
shassen <shassen@tamu.edu>
2019-09-22 12:16:32 -0500

    made a braking map

--- brake_data_gradient.m
+++ brake_data_gradient.m
@@ -7,7 +7,7 @@ startmin = 5;
 endmin = 10;
 
 %% Acceleration Data For Variety of velocities for 0% Brake 
-velocity = [2.399589, 4.471362, 6.448318, 8.559670, 11.072673, 13.000194, 15.507926, 17.218624];  % m/s
+velocity_brake0 = [2.399589, 4.471362, 6.448318, 8.559670, 11.072673, 13.000194, 15.507926, 17.218624];  % m/s
 
 inFile = 'brake_0_5mph/_vectornav_veltest_msg.data.txt';
 data = load(inFile, '-ascii');
@@ -76,7 +76,7 @@ brake40mph = mean(sort_filtered_data(startmin:endmin,:));
 acceleration0 = [brake5mph, brake10mph, brake15mph, brake20mph, brake25mph, brake30mph, brake35mph, brake40mph];
 
 figure(1)
-plot(velocity, acceleration0, 'color', [0.0 0.0 0.0], 'linestyle', '-', 'marker', 'o')
+plot(velocity_brake0, acceleration0, 'color', [0.0 0.0 0.0], 'linestyle', '-', 'marker', 'o')
 xlabel('Velocity (m/s)')
 ylabel('Acceleration (m/s^2)')
 title('Braking Speed-based Acceleration Map')
@@ -84,7 +84,7 @@ grid on
 hold on
 
 %% Acceleration Data For Variety of velocities for 40% Brake 
-velocity = [2.332692, 4.290472, 6.482032, 8.636277, 10.623586, 12.743763, 15.416848, 17.320083];  % m/s
+velocity_brake40 = [2.332692, 4.290472, 6.482032, 8.636277, 10.623586, 12.743763, 15.416848, 17.320083];  % m/s
 
 inFile = 'brake_40_5mph/_vectornav_veltest_msg.data.txt';
 data = load(inFile, '-ascii');
@@ -150,11 +150,11 @@ sort_filtered_data = sort(y);
 
 brake40mph = mean(sort_filtered_data(startmin:endmin,:));
 
-acceleration40 = [brake5mph, brake10mph, brake15mph, brake20mph, brake25mph, brake30mph, brake35mph, brake40mph];
+acceleration_brake40 = [brake5mph, brake10mph, brake15mph, brake20mph, brake25mph, brake30mph, brake35mph, brake40mph];
 
-plot(velocity, acceleration40, 'color', [1 0.5 0.5], 'linestyle', '-', 'marker', 'o')
+plot(velocity_brake40, acceleration_brake40, 'color', [1 0.5 0.5], 'linestyle', '-', 'marker', 'o')
 %% Acceleration Data For Variety of velocities for 50% Brake 
-velocity = [2.255553, 4.345825, 6.759151, 8.636277, 10.623586, 12.743763, 15.416848, 17.320083];  % m/s
+velocity_brake50 = [2.255553, 4.345825, 6.759151, 8.636277, 10.623586, 12.743763, 15.416848, 17.320083];  % m/s
 
 inFile = 'brake_50_5mph/_vectornav_veltest_msg.data.txt';
 data = load(inFile, '-ascii');
@@ -228,12 +228,12 @@ sort_filtered_data = sort(y);
 
 brake40mph = mean(sort_filtered_data(startmin:endmin,:));
 
-acceleration50 = [brake5mph, brake10mph, brake15mph, brake20mph, brake25mph, brake30mph, brake35mph, brake40mph];
+acceleration_brake50 = [brake5mph, brake10mph, brake15mph, brake20mph, brake25mph, brake30mph, brake35mph, brake40mph];
 
-plot(velocity, acceleration50, 'color', [1 0.0 0.0], 'linestyle', '-', 'marker', 'o')
+plot(velocity_brake50, acceleration_brake50, 'color', [1 0.0 0.0], 'linestyle', '-', 'marker', 'o')
 
 %% Acceleration Data For Variety of velocities for 60% Brake 
-velocity = [1.949396, 4.456033, 6.263209, 8.291727, 10.666364, 12.900174, 15.004222, 17.342080];  % m/s
+velocity_brake60 = [1.949396, 4.456033, 6.263209, 8.291727, 10.666364, 12.900174, 15.004222, 17.342080];  % m/s
 
 inFile = 'brake_60_5mph/_vectornav_veltest_msg.data.txt';
 data = load(inFile, '-ascii');
@@ -307,11 +307,11 @@ sort_filtered_data = sort(y);
 
 brake40mph = mean(sort_filtered_data(startmin:endmin,:));
 
-acceleration60 = [brake5mph, brake10mph, brake15mph, brake20mph, brake25mph, brake30mph, brake35mph, brake40mph];
+acceleration_brake60 = [brake5mph, brake10mph, brake15mph, brake20mph, brake25mph, brake30mph, brake35mph, brake40mph];
 
-plot(velocity, acceleration60, 'color', [0.5764 0.0 0.0], 'linestyle', '-', 'marker', 'o')
+plot(velocity_brake60, acceleration_brake60, 'color', [0.5764 0.0 0.0], 'linestyle', '-', 'marker', 'o')
 %% Acceleration Data For Variety of velocities for 70% Brake 
-velocity = [2.370531, 4.337160, 6.588608, 8.268588, 10.751820, 12.916797, 15.015370, 17.322462];  % m/s
+velocity_brake70 = [2.370531, 4.337160, 6.588608, 8.268588, 10.751820, 12.916797, 15.015370, 17.322462];  % m/s
 
 inFile = 'brake_70_5mph/_vectornav_veltest_msg.data.txt';
 data = load(inFile, '-ascii');
@@ -385,12 +385,12 @@ sort_filtered_data = sort(y);
 
 brake40mph = mean(sort_filtered_data(startmin:endmin,:));
 
-acceleration70 = [brake5mph, brake10mph, brake15mph, brake20mph, brake25mph, brake30mph, brake35mph, brake40mph];
+acceleration_brake70 = [brake5mph, brake10mph, brake15mph, brake20mph, brake25mph, brake30mph, brake35mph, brake40mph];
 
-plot(velocity, acceleration70, 'color', [0.0 1.0 0.0], 'linestyle', '-', 'marker', 'o')
+plot(velocity_brake70, acceleration_brake70, 'color', [0.0 1.0 0.0], 'linestyle', '-', 'marker', 'o')
 
 %% Acceleration Data For Variety of velocities for 80% Brake 
-velocity = [2.103452, 4.275421, 6.515093, 8.560335, 10.620332, 13.058321, 15.060000, 17.381358];  % m/s
+velocity_brake80 = [2.103452, 4.275421, 6.515093, 8.560335, 10.620332, 13.058321, 15.060000, 17.381358];  % m/s
 
 inFile = 'brake_80_5mph/_vectornav_veltest_msg.data.txt';
 data = load(inFile, '-ascii');
@@ -465,11 +465,11 @@ sort_filtered_data = sort(y);
 
 brake40mph = mean(sort_filtered_data(startmin:endmin,:));
 
-acceleration80 = [brake5mph, brake10mph, brake15mph, brake20mph, brake25mph, brake30mph, brake35mph, brake40mph];
+acceleration_brake80 = [brake5mph, brake10mph, brake15mph, brake20mph, brake25mph, brake30mph, brake35mph, brake40mph];
 
-plot(velocity, acceleration80, 'color', [0.0 0.5647 0.0], 'linestyle', '-', 'marker', 'o')
+plot(velocity_brake80, acceleration_brake80, 'color', [0.0 0.5647 0.0], 'linestyle', '-', 'marker', 'o')
 %% Acceleration Data For Variety of velocities for 90% Brake 
-velocity = [2.103452, 4.229488, 6.698545, 8.387588, 10.684664, 12.880022, 15.219108, 17.439246];  % m/s
+velocity_brake90 = [2.103452, 4.229488, 6.698545, 8.387588, 10.684664, 12.880022, 15.219108, 17.439246];  % m/s
 
 inFile = 'brake_90_5mph/_vectornav_veltest_msg.data.txt';
 data = load(inFile, '-ascii');
@@ -535,11 +535,11 @@ sort_filtered_data = sort(y);
 
 brake40mph = mean(sort_filtered_data(startmin:endmin,:));
 
-acceleration90 = [brake5mph, brake10mph, brake15mph, brake20mph, brake25mph, brake30mph, brake35mph, brake40mph];
+acceleration_brake90 = [brake5mph, brake10mph, brake15mph, brake20mph, brake25mph, brake30mph, brake35mph, brake40mph];
 
-plot(velocity, acceleration90, 'color', [0 0.0 1.0], 'linestyle', '-', 'marker', 'o')
+plot(velocity_brake90, acceleration_brake90, 'color', [0 0.0 1.0], 'linestyle', '-', 'marker', 'o')
 %% Acceleration Data For Variety of velocities for 100% Brake 
-velocity = [1.928696, 4.471362, 6.240145, 8.445275, 10.433095, 12.800073, 15.110204, 17.132518];  % m/s
+velocity_brake100 = [1.928696, 4.471362, 6.240145, 8.445275, 10.433095, 12.800073, 15.110204, 17.132518];  % m/s
 
 inFile = 'brake_100_5mph/_vectornav_veltest_msg.data.txt';
 data = load(inFile, '-ascii');
@@ -608,9 +608,45 @@ sort_filtered_data = sort(y);
 brake40mph = mean(sort_filtered_data(startmin:endmin,:));
 
 
-acceleration100 = [brake5mph, brake10mph, brake15mph, brake20mph, brake25mph, brake30mph, brake35mph, brake40mph];
+acceleration_brake100 = [brake5mph, brake10mph, brake15mph, brake20mph, brake25mph, brake30mph, brake35mph, brake40mph];
 
-plot(velocity, acceleration100, 'color', [0 0.7490 1.0], 'linestyle', '-', 'marker', 'o')
+plot(velocity_brake100, acceleration_brake100, 'color', [0 0.7490 1.0], 'linestyle', '-', 'marker', 'o')
 legend('0% Brake', '40% Brake', '50% Brake', '60% Brake','70% Brake','80% Brake','90% Brake','100% Brake')
 
-%% 3D plotting stuff
+%% Developing vectors for surface fitting
+% speed = [velocity_brake0, velocity_brake50, velocity_brake60, velocity_brake70,...
+%         velocity_brake80, velocity_brake90, velocity_brake100];
+% acceleration = [acceleration0, acceleration_brake50, acceleration_brake60, acceleration_brake70,...
+%                 acceleration_brake80, acceleration_brake90, acceleration_brake100];
+% brake0 = 0*ones(size(acceleration0));
+% brake40 = -40*ones(size(acceleration_brake40));
+% brake50 = -50*ones(size(acceleration_brake50));
+% brake60 = -60*ones(size(acceleration_brake60));
+% brake70 = -70*ones(size(acceleration_brake70));
+% brake80 = -80*ones(size(acceleration_brake80));
+% brake90 = -90*ones(size(acceleration_brake90));
+% brake100 = -100*ones(size(acceleration_brake100));
+% 
+% brake = [brake0, brake50, brake60, brake70, brake80, brake90, brake100];
+% brake1 = 0.01*brake;
+
+
+speed = [velocity_brake0(1:5), velocity_brake50(1:5), velocity_brake60(1:5), velocity_brake70(1:5),...
+        velocity_brake80(1:5), velocity_brake90(1:5), velocity_brake100(1:5)];
+acceleration = [acceleration0(1:5), acceleration_brake50(1:5), acceleration_brake60(1:5), acceleration_brake70(1:5),...
+                acceleration_brake80(1:5), acceleration_brake90(1:5), acceleration_brake100(1:5)];
+brake0 = 0*ones(size(acceleration0));
+brake40 = -40*ones(size(acceleration_brake40));
+brake50 = -50*ones(size(acceleration_brake50));
+brake60 = -60*ones(size(acceleration_brake60));
+brake70 = -70*ones(size(acceleration_brake70));
+brake80 = -80*ones(size(acceleration_brake80));
+brake90 = -90*ones(size(acceleration_brake90));
+brake100 = -100*ones(size(acceleration_brake100));
+
+brake = [brake0(1:5), brake50(1:5), brake60(1:5), brake70(1:5), brake80(1:5), brake90(1:5), brake100(1:5)];
+brake1 = 0.01*brake;
+
+% createFit(speed, throttle, acceleration)
+cftool
+
--- myPlotter.py
+++ myPlotter.py
@@ -57,7 +57,7 @@ for i in range(len(listdir_)):
 	
 	name_,data_ = storeProccessed(listdir_,i)
 
-	long_ndx = returnPltPos(name_,"_vehicle_odom2_msg.x")
+	long_ndx = returnPltPos(name_,"_vehicle_odom2_msg.x)
 	lat_ndx = returnPltPos(name_,"_vehicle_odom2_msg.y")
 	height_ndx = returnPltPos(name_, "_vectornav_gps_msg.LLA.z")
 
@@ -74,47 +74,50 @@ for i in range(len(listdir_)):
 	brake_ndx = returnPltPos(name_,"_as_rx_brake_cmd_msg.command")
 
 	manualthrottle_ndx = returnPltPos(name_,"_parsed_tx_accel_rpt_msg.manual_input")
-	throttleout_ndx = manualthrottle_ndx = returnPltPos(name_,"_parsed_tx_accel_rpt_msg.output")
+	throttleout_ndx  = returnPltPos(name_,"_parsed_tx_accel_rpt_msg.output")
+	throttlerptcom_ndx = returnPltPos(name_,"_parsed_tx_accel_rpt_msg.command")
 	brakeout_ndx = returnPltPos(name_,"_parsed_tx_brake_rpt_msg.command")
 
 	linear_acc = np.gradient(data_[vecvelocity_ndx][:,1],0.02)
 	linear_acc_filter  = np.array(scipy.signal.savgol_filter(linear_acc, 51,3))
 
-
 	linear_acc_time = np.array(data_[vecvelocity_ndx][:,0])
 	linear_acc_ndx = np.column_stack((linear_acc_time, linear_acc))
 	linear_acc_filter_ndx = np.column_stack((linear_acc_time, linear_acc_filter))
 
-
 	try:	
 		output_file(listdir_[i][2:-1]+".html")
-		output_file(listdir_[i][2:-1]+".html")
 
-		p1 = figure(title=listdir_[i], x_axis_label='longitude', y_axis_label='latitude')
+		p1 = figure(title=listdir_[i], x_axis_label='longitude (m)', y_axis_label='latitude (m)')
 		p1.line(data_[long_ndx][:,1] - data_[long_ndx][0,1], data_[lat_ndx][:,1] - data_[lat_ndx][0,1],  legend="Path Driven",line_width=2, line_color="red")
 
-		p2 = figure(title=listdir_[i], x_axis_label='time', y_axis_label='latitude')
+		p2 = figure(title=listdir_[i], x_axis_label='time (sec)', y_axis_label='acceleration (m/s^2)')
 		p2.line(data_[acc_x_ndx][:,0], data_[acc_x_ndx][:,1], legend="Acceleration X",line_width=2, line_color="red")
 		p2.line(data_[acc_y_ndx][:,0], data_[acc_y_ndx][:,1], legend="Acceleration Y",line_width=2, line_color="blue")
 		p2.line(data_[acc_z_ndx][:,0], data_[acc_z_ndx][:,1], legend="Acceleration Z",line_width=2, line_color="green")
 
-		p3 = figure(title=listdir_[i], x_axis_label='time', y_axis_label='velocity (m/s)')
+		p3 = figure(title=listdir_[i], x_axis_label='time (sec)', y_axis_label='velocity (m/s) or acceleration (m/s^2)')
 		p3.line(data_[vecvelocity_ndx][:,0], data_[vecvelocity_ndx][:,1], legend="VectorNav Velocity",line_width=2, line_color="red")
-		# p3.line(data_[pacvelocity_ndx][:,0], data_[pacvelocity_ndx][:,1], legend="PACMod Velocity",line_width=2, line_color="blue")
-		# p3.line(data_[manualthrottle_ndx][:,0], data_[manualthrottle_ndx][:,1], legend="Manual Throttle",line_width=2, line_color="blue")
-		# p3.line(linear_acc_ndx[:,0], linear_acc_ndx[:,1], legend="Linear Acceleration",line_width=2, line_color="cyan")
-		p3.line(linear_acc_filter_ndx[:,0], linear_acc_filter_ndx[:,1], legend="Filtered Linear Acceleration",line_width=2, line_color="blue")
+		# p3.line(data_[throttle_ndx][:,0], data_[throttle_ndx][:,1], legend="Throttle Command",line_width=2, line_color="blue")
+
+		# p3.line(linear_acc_ndx[:,0], linear_acc_ndx[:,1], legend="Linear Acceleration",line_width=2, line_color="green")
+		p3.line(linear_acc_filter_ndx[:,0], linear_acc_filter_ndx[:,1], legend="Filtered Linear Acceleration",line_width=2, line_color="cyan")
+
+		# p3.line(data_[throttleout_ndx][:,0], data_[throttleout_ndx][:,1], legend="Throttle Output",line_width=2, line_color="cyan")
+		# p3.line(data_[pacvelocity_ndx][:,0], data_[pacvelocity_ndx][:,1], legend="PACMod Velocity",line_width=2, line_color="green")
 		p3.line(data_[brake_ndx][:,0], data_[brake_ndx][:,1], legend="Brake Command",line_width=2, line_color="green")
 		# p3.line(data_[brakeout_ndx][:,0], data_[brakeout_ndx][:,1], legend="Brake Output",line_width=2, line_color="black")
-		
-		# p4 = figure(title=listdir_[i], x_axis_label='time', y_axis_label = 'Brake %')
-		# p4.line(data_[brakeout_ndx][:,0], data_[brakeout_ndx][:,1]*100, legend="Brake Output",line_width=2, line_color="red")
-		# p4.line(data_[brake_ndx][:,0], data_[brake_ndx][:,1]*100, legend="Brake Command",line_width=2, line_color="blue")
 
-		curr = gridplot([[p1, p3, p2]])
+		# p4 = figure(title=listdir_[i], x_axis_label='time', y_axis_label='throttle')
+		# p4.line(data_[throttle_ndx][:,0], data_[throttle_ndx][:,1]*100, legend="Throttle % Command",line_width=2, line_color="red")
+		# p4.line(data_[throttleout_ndx][:,0], data_[throttleout_ndx][:,1]*100, legend="Throttle % Output",line_width=2, line_color="blue")
+		# p4.line(data_[throttlerptcom_ndx][:,0], data_[throttlerptcom_ndx][:,1]*100, legend="Throttle % Report Command",line_width=2, line_color="green")
+
+		curr = gridplot([[ p3]])
 		show(curr)
-
 
 	except Exception:
 		print i, listdir_[i], traceback.format_exc()		
 		pass
+
+	
